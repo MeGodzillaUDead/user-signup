5ea1893ba02fe4e1490cef21b4f564edd01648e1
Jonathan Riezman <riezman@gmail.com>
2017-09-26 18:36:51 -0500

    refactored code so NO MATCH error goes by the confirm password field

--- main.py
+++ main.py
@@ -18,14 +18,20 @@ def validate_basic(entry):
 	else:
 		return True
 		
-def validate_password(entry1, entry2):
+'''def validate_password(entry1, entry2):
 	if validate_basic(entry1) == True:
 		if entry1 != entry2:
 			return "Passwords do not match."
 		else:
 			return True
 	else:
-		return validate_basic(entry1)
+		return validate_basic(entry1)'''
+		
+def match(entry1, entry2):
+	if entry1 == entry2:
+		return True
+	else:
+		return "Passwords do not match."
 		
 def validate_email(entry):
 	if validate_basic(entry) == True:
@@ -43,8 +49,10 @@ def index():
 	ee = request.args.get('ee')
 	n_val = request.args.get('n_val')
 	e_val = request.args.get('e_val')
+	mc = request.args.get('mc')
 
 # wtf doesn't this for loop work?!?!	
+#	errors = [ue, pe, ee]
 # 	for item in errors:
 #		if item == None:
 #			item = ""
@@ -59,6 +67,8 @@ def index():
 		n_val = ""
 	if e_val == None or ee not in ["True", ""]:
 		e_val = ""
+	if mc == None:
+		mc = ""
 	
 	return render_template('signup-form.html', title='New User Sign Up',
 		ue = ue,
@@ -66,6 +76,7 @@ def index():
 		ee = ee,
 		nv = n_val,
 		ev = e_val,
+		mc = mc
 		)
 
 @app.route("/welcome", methods=['POST'])
@@ -78,20 +89,22 @@ def welcome():
 	
 	# get validation True or error message for each field
 	uname_error = validate_basic(user_name)
-	pw_error = validate_password(pw, cpw)
+	pw_error = validate_basic(pw)
+	match_check = match(pw, cpw)
 	if email == "":
 		email_error = True
 	else:
 		email_error = validate_email(email)
 	
-	if uname_error == pw_error == email_error == True:
+	if uname_error == pw_error == match_check == email_error == True:
 	# landing page render statement
 		return render_template('welcome.html', title='Landing Page', name=user_name)
 	else:
-		return redirect("/?ue={0}&pe={1}&ee={2}&n_val={3}&e_val={4}".format(uname_error, 
+		return redirect("/?ue={0}&pe={1}&ee={2}&n_val={3}&e_val={4}&mc={5}".format(uname_error, 
 		pw_error, 
 		email_error, 
 		user_name, 
-		email))
+		email,
+		match_check))
 		
 app.run()
